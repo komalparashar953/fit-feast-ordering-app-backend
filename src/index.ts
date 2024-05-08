aec14e8701e9e92d5f6cf8e588541f5a3f161850
KOMAL1108 <parashark953@gmail.com>
2024-05-08 21:01:19 +0530

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from './routes/myUserRoute';
 import { v2 as cloudinary } from "cloudinary";
-
+import restaurantRoute from "./routes/RestaurantRoute";
 import MyRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -26,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) =>{
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", MyRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000");
