32bb19d02c5ebd896d94127ba957b2d44d9ba00e
KOMAL1108 <parashark953@gmail.com>
2024-05-21 14:37:44 +0530

    added keys

--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.get("/", jwtCheck, jwtParse, OrderController.getMyOrders);
 
-router.post("/checkout/create-checkout-session", jwtCheck, jwtParse, OrderController.createCheckoutSession);
+router.post(
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession
+);
 
 router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
